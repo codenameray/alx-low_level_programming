d68be6fad0014286fa84b1625ae181a97c41c0b8
Raymond Adesioye <adesioyeray@gmail.com>
2021-06-17 12:46:46 -0700

    Correcting errors

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * return: Always 0 (Successful)
- */
+* main - Entry point
+*
+* Return: Always 0 (Successful)
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
