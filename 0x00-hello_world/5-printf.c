76647da476acfb8a2c423b9c648ac4152eb17749
Raymond Adesioye <adesioyeray@gmail.com>
2021-06-17 12:39:54 -0700

    Still on task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Entry point
- * 
- * return: 0 (Successful)
+ *
+ * return: Always 0 (Successful)
  */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
