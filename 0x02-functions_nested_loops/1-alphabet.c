326f9dabac13db31b4a9eded68a3adca02289b54
Raymond Adesioye <adesioyeray@gmail.com>
2021-06-22 12:11:36 -0700

    Task 1-print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* main - check the code for ALX School students.
+*
+* Return: Always 0.
+*/
+/*function declaration*/
+void print_alphabet(void);
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+void print_alphabet(void)
+{
+char c;
+for(c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return;
+}
