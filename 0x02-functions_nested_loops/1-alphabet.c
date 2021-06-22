812b690abe079945ebbf5657dcc5be7050c356ec
Raymond Adesioye <adesioyeray@gmail.com>
2021-06-22 14:08:17 -0700

    still

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,10 +11,10 @@ int main(void)
 print_alphabet();
 return (0);
 }
-void print_alphabet(void)
+print_alphabet(void)
 {
 char c;
-for(c = 'a'; c <= 'z'; c++)
+for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 }
