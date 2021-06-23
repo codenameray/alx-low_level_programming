95740e2714efbd3af2f71e85a8e95facfe7082d2
Raymond Adesioye <adesioyeray@gmail.com>
2021-06-23 04:52:21 -0700

    betty changes to 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,13 +6,13 @@
 */
 int _islower(int c)
 {
-  char lower = 'a';
-  for (lower = 'a'; lower <= 'z'; lower++)
-    {
-      if (c == lower)
-	{
-	  return (1);
-	}
-    }
-  return (0);
+char lower = 'a';
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+if (c == lower)
+{
+return (1);
+}
+}
+return (0);
 }
